cac1f4c1917933411d7737803a7485af63e1819b
Dmitry Olegovich Sorokin (@sorydima) <sorydima@gmail.com>
2025-03-21 23:10:22 +0300

    flutter_distributor

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/sorydev/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/sorydev/Documents/GitHub/Katya-"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.3.22"
+export "FLUTTER_BUILD_NUMBER=1027"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
